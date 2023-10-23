f2197b43b709d883f0334fe79027f7c124049975
firjatullahwira <148465019+firjatullahwira@users.noreply.github.com>
2023-10-23 22:25:38 +0700

    Add files via upload

---
+++ tugas1 cari file.py
@@ -0,0 +1,7 @@
+nama_file = "data.txt"
+daftar_file_di_server = ["file1.txt", "file2.txt", "data.txt", "file3.txt"]
+
+if nama_file in daftar_file_di_server:
+    print("File sudah ada")
+else:
+    print("File belum ada")
